69b5e9d9d9aa261e284f6017753a214ce9d78cca
Anirban <pairaanirban123@gmail.com>
2022-08-25 15:10:48 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
 const customerRoute = require("./router/customerRoute");
 app.use(express.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://anirban:1234@cluster0.40kmx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(process.env.DATA_BASE)
   .then(() => {
     console.log("DATABASE is connected");
   })
@@ -16,10 +16,10 @@ mongoose
 
 app.use("/", customerRoute);
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "server is running",
-  });
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./build/index.html"));
 });
 
 app.listen(8000, () => console.log("server is runnig post 8000"));
